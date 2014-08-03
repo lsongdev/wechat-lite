d684471c0a533e0f300063d9b77ebaba22e0f749
Lsong <song940@gmail.com>
2014-08-03 23:05:44 +0800

    reply message .

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var xmlParser = require('express-xml');
 var wechat = require('./wechat');
@@ -13,71 +12,40 @@ app.use(function(req, res, next){
 app.use(xmlParser);
 
 app.use(wechat('token', function(req, res){
-	console.log(req.msg);
-	var msg = req.msg;
-	switch(msg.MsgType){
-		case 'text':
-			var ToUserName = msg['ToUserName'];
-			msg['ToUserName'] = msg['FromUserName'];
-			msg['FromUserName'] = ToUserName;
-			res.reply(msg);
-			break;
-		case 'image':
-			res.reply({
-				ToUserName: msg['FromUserName'],
-				FromUserName: msg['ToUserName'],
-				CreateTime: +new Date,
-				MsgType: 'image',
-				Image: { MediaId: msg['MediaId'] }
-			});
-			break;
-		case 'voice':
-			res.reply({
-				ToUserName: msg['FromUserName'],
-				FromUserName: msg['ToUserName'],
-				CreateTime: +new Date,
-				MsgType: 'voice',
-				Voice: { MediaId: msg['MediaId'] }
-			});
-			break;
-		case 'video':
-			res.reply({
-				ToUserName: msg['FromUserName'],
-				FromUserName: msg['ToUserName'],
-				CreateTime: +new Date,
-				MsgType: 'video',
-				Video: { 
-					MediaId: msg['MediaId'],
-					Title: '',
-					Description: ''
-				}
-			});
-			break;	
-		case 'location':
-			console.log(msg['Location_X']);
-			console.log(msg['Location_Y']);
-			console.log(msg['Scale']);
-			console.log(msg['Label']);
-			break;	
-		case 'event':
-			var ev = msg['Event'];
-			switch(ev){
-				case 'subscribe':
-					res.reply({
-						ToUserName: msg['FromUserName'],
-						FromUserName: msg['ToUserName'],
-						CreateTime: +new Date,
-						MsgType: 'text',
-						Content: 'Welcome join us !'
-					});
-					break;
-				case 'unsubscribe':
-					console.log('bye');
-					break;
-			}
-			break;
-	}
+	console.log(req.msg);	
+	//res.reply('hehe');
+	//res.reply('text', 'hi');
+	//res.reply('image', 'kDEwlMDh4a_LXggpuJfJzbJDmb4i7g1hxLiCXDAgCH0MEaTZeKLDfx7jimi0GTPN');
+	//res.reply('voice', 'bvnweVYYmeTmvpOZ-CG7KHeZpzQH9bvFcS-8TV9xgwPZGCSTClv6K2ccvZEYCnwi');
+	//res.reply('video', {
+	//	title: 'video',
+	//	description: 'this is a video',
+	//	MediaId: 'DBVFRIj29LB2hxuYpc0R6VLyxwgyCHZPbRj_IIs6YaGhutyXUKtFSDcSCPeoqUYr'
+	//});
+	//res.reply('music', {
+	//	title: '小苹果',
+	//	description: '你是我的小呀小苹果',
+	//	pic: 'kDEwlMDh4a_LXggpuJfJzbJDmb4i7g1hxLiCXDAgCH0MEaTZeKLDfx7jimi0GTPN',
+	//	url: 'http://119.2.14.188:8085/F/3774FCD95DB93101CCB4B910ADC33FA3F586BACB/BEB3849722A693AC480EF1038C2D7E8AF0A38C80/txkj_url/yinyueshiting.baidu.com/data2/music/121859548/1201250291407038461128.mp3',
+	//	hq_url: 'http://119.2.14.188:8085/F/3774FCD95DB93101CCB4B910ADC33FA3F586BACB/BEB3849722A693AC480EF1038C2D7E8AF0A38C80/txkj_url/yinyueshiting.baidu.com/data2/music/121859548/1201250291407038461128.mp3'
+	//});
+	res.reply('news', [
+	//	{
+	//		title: 'baidu',
+	//		description: 'baidu sb!',
+	//		pic: 'http://www.baidu.com/img/baidu_sylogo1.gif',
+	//		url: 'http://baidu.com'
+	//	},
+		{
+			title: 'sina',
+			description: 'sexna',
+			pic: 'http://i2.sinaimg.cn/dy/deco/2013/0329/logo/LOGO_2x.png',
+			url: 'http://sina.com'
+		}
+	]);
 }));
 
 
-app.listen(3001);
+var server = app.listen(3001, function(){
+	console.log("server is running at %s", server.address().port);
+});
--- wechat.js
+++ wechat.js
@@ -44,12 +44,68 @@ var wechat = function(token, callback){
 		checkSignature(token, req, res, function(){
 			var body = req.body;
 			var data = req.body['xml'];
-			res.reply = function(msg){
-				var xml = jstoxml.toXML({ xml: msg });
+			var msg = parseMsg(data);
+			res.reply = function(type, message){
+				var replyMsg = {
+					FromUserName: msg['ToUserName'],
+					ToUserName	: msg['FromUserName'],
+					CreateTime	: parseInt(+new Date / 1000)
+				};
+				switch(type){
+					case 'text':
+						replyMsg['Content'] = message;
+						break;
+					case 'image':
+						replyMsg['Image'] = {
+							MediaId: message
+						};
+						break;
+					case 'voice':
+						replyMsg['Voice'] = {
+							MediaId: message
+						};
+						break;
+					case 'music':
+						replyMsg['Music'] = {
+							Title: message['title'],
+              Description: message['description'],
+							ThumbMediaId: message['pic'],
+							MusicUrl: message['url'],
+							HQMusicUrl: message['hq_url']
+						};
+						break;
+					case 'video':
+						replyMsg['Video'] = {
+							Title: message['title'],
+							Description: message['description'],
+							MediaId: message['MediaId']
+						};
+						break;
+					case 'news':
+						var articles = [];
+						message.forEach(function(article){
+							var item = {
+								Title: article['title'],
+								Description: article['description'],
+								PicUrl: article['pic'],
+								Url: article['url']
+							};
+							articles.push({ item: item });
+						});
+						replyMsg['Articles'] = articles;
+						replyMsg['ArticleCount'] = articles.length;
+						break;
+					default:
+						message = type;
+						type = 'text';
+						replyMsg['Content'] = message;
+						break;
+				}
+				replyMsg['MsgType'] = type;
+				var xml = jstoxml.toXML({ xml: replyMsg });
 				console.log(xml);
 				res.send(xml);
 			};
-			var msg = parseMsg(data);
 			var seed  = msg.CreateTime;
 			if(!~msgQueue.indexOf(seed)){
 				msgQueue.push(seed);
