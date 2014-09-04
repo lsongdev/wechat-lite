c3560c3e0210df8407a788a47bb1fd6bf81124f6
Lsong <song940@gmail.com>
2014-09-04 17:18:33 +0800

    fix a bug .

--- libs/wechat.js
+++ libs/wechat.js
@@ -39,67 +39,64 @@ var checkSignature = function(token, req, res, callback){
   }
 };
 
-var reply = function(msg){
-  return function(type, message){
-    var replyMsg = {
-      FromUserName: msg['ToUserName'],
-      ToUserName  : msg['FromUserName'],
-      CreateTime  : parseInt(+new Date / 1000)
-    };
-    switch(type){
-      case 'text':
-        replyMsg['Content'] = message;
-        break;
-      case 'image':
-        replyMsg['Image'] = {
-          MediaId: message
-        };
-        break;
-      case 'voice':
-        replyMsg['Voice'] = {
-          MediaId: message
-        };
-        break;
-      case 'music':
-        replyMsg['Music'] = {
-          Title: message['title'],
-          Description: message['description'],
-          ThumbMediaId: message['pic'],
-          MusicUrl: message['url'],
-          HQMusicUrl: message['hq_url']
-        };
-        break;
-      case 'video':
-        replyMsg['Video'] = {
-          Title: message['title'],
-          Description: message['description'],
-          MediaId: message['MediaId']
-        };
-        break;
-      case 'news':
-        var articles = [];
-        message.forEach(function(article){
-          var item = {
-            Title: article['title'],
-            Description: article['description'],
-            PicUrl: article['pic'],
-            Url: article['url']
-          };
-          articles.push({ item: item });
-        });
-        replyMsg['Articles'] = articles;
-        replyMsg['ArticleCount'] = articles.length;
-        break;
-      default:
-        message = type;
-        type = 'text';
-        replyMsg['Content'] = message;
-        break;
-    }
-    replyMsg['MsgType'] = type;
-    var xml = jstoxml.toXML({ xml: replyMsg });
-    res.send(xml);
+var getReplyMsg = function(msg, type, message){
+  var replyMsg = {
+    FromUserName: msg['ToUserName'],
+    ToUserName  : msg['FromUserName'],
+    CreateTime  : parseInt(+new Date / 1000)
   };
+  switch(type){
+    case 'text':
+      replyMsg['Content'] = message;
+      break;
+    case 'image':
+      replyMsg['Image'] = {
+        MediaId: message
+      };
+      break;
+    case 'voice':
+      replyMsg['Voice'] = {
+        MediaId: message
+      };
+      break;
+    case 'music':
+      replyMsg['Music'] = {
+        Title: message['title'],
+        Description: message['description'],
+        ThumbMediaId: message['pic'],
+        MusicUrl: message['url'],
+        HQMusicUrl: message['hq_url']
+      };
+      break;
+    case 'video':
+      replyMsg['Video'] = {
+        Title: message['title'],
+        Description: message['description'],
+        MediaId: message['MediaId']
+      };
+      break;
+    case 'news':
+      var articles = [];
+      message.forEach(function(article){
+        var item = {
+          Title: article['title'],
+          Description: article['description'],
+          PicUrl: article['pic'],
+          Url: article['url']
+        };
+        articles.push({ item: item });
+      });
+      replyMsg['Articles'] = articles;
+      replyMsg['ArticleCount'] = articles.length;
+      break;
+    default:
+      message = type;
+      type = 'text';
+      replyMsg['Content'] = message;
+      break;
+  }
+  replyMsg['MsgType'] = type;
+  return replyMsg;
 };
 
 var wechat = function(token, callback){
@@ -108,7 +105,11 @@ var wechat = function(token, callback){
       var body = req.body;
       var data = req.body['xml'];
       var msg = parseMsg(data);
-      res.reply = reply(msg);
+      res.reply = function(type, message){
+        var replyMsg = getReplyMsg(msg, type, message);
+        var xml = jstoxml.toXML({ xml: replyMsg });
+        res.send(xml);
+      };
       var seed  = msg.CreateTime;
       if(!~msgQueue.indexOf(seed)){
         msgQueue.push(seed);
