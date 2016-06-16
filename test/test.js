294735fadf0806977089ea62857a4a6b43281466
Lsong <song940@gmail.com>
2016-06-16 19:42:47 +0800

    add template and custom message

--- test/test.js
+++ test/test.js
@@ -3,13 +3,13 @@ const assert = require('assert');
 const WeChat = require('../');
 
 var wx = new WeChat({
-  appId     : 'wx779ea5a9af3d5d09',
-  appSecret : 'ea6eea9459b57da58dbc673d1f52c4df'
+  appId     : 'wxfb9dc00461bcef17',
+  appSecret : '21f48e4c7c68683b681ca81ba60d3190'
 });
 
 describe('wechat api', function() {
   
-  var openId = 'ogpecs31cnl2zcr7bkG9fdVyu1c8';
+  var openId = 'ozwcHuC792LGHpQ0dYFlYoA2Uh_c';
   
   before(function(done){
     wx.getToken().then(function(token){
@@ -37,27 +37,50 @@ describe('wechat api', function() {
   
   it('get user', function(done) {
     wx.getUser(wx.token, openId).then(function(user){
+      // console.log(user);
       assert.ok(user.openid);
       assert.ifError(user.errcode, user.errmsg);
       done();
     });
   });
   
-  it('menu list', function(done) {
-    wx.menu_list(wx.token).then(function(res){
-      assert.ok(res.is_menu_open);
-      assert.ok(res.selfmenu_info);
+  it('send template message', function(done) {
+    var templateId = 'zVwzW_Sm7Ln8mrf2a74-3XgWbDMPYbXVcjEbf-aA75o';
+    var data = {
+      name: 'Lsong',
+      content: 'test'
+    };
+  
+    wx.template_send(wx.token, openId, templateId, data, 'https://lsong.org').then(function(res){
       assert.ifError(res.errcode, res.errmsg);
+      assert.ok(res.msgid)
       done();
     });
   });
   
-  it('set user remark', function(done) {
-    wx.user_remark(wx.token, openId, 'remark').then(function(res){
+  it('send custom message', function(done) {
+    wx.custom_send(wx.token, openId, 'text', { content: 'test' }).then(function(res){
       assert.ifError(res.errcode, res.errmsg);
       done();
     });
   });
+  //
+  // it('menu list', function(done) {
+  //   wx.menu_list(wx.token).then(function(res){
+  //     assert.ok(res.is_menu_open);
+  //     assert.ok(res.selfmenu_info);
+  //     assert.ifError(res.errcode, res.errmsg);
+  //     done();
+  //   });
+  // });
+  //
+  // it('set user remark', function(done) {
+  //   wx.user_remark(wx.token, openId, 'remark').then(function(res){
+  //     assert.ifError(res.errcode, res.errmsg);
+  //     done();
+  //   });
+  // });
+  
   
 
 });
