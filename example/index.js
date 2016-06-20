3a4d8468309a5a5e5ad6083473b0e998015a5461
Lsong <song940@gmail.com>
2016-06-20 14:23:22 +0800

    modify

--- example/index.js
+++ example/index.js
@@ -3,15 +3,12 @@ const WeChat = require('../');
 var api = new WeChat({
   appId     : 'wx779ea5a9af3d5d09',
   appSecret : 'ea6eea9459b57da58dbc673d1f52c4df'
+}, function(done){
+  this.token().then(function(token){
+    done(token.access_token);
+  });
 });
 
-// api
-// .token()
-// .then((x) => x.access_token)
-// .then(api.ticket())
-// .then(function(ticket){
-//   console.log(ticket);
-// })
-
-api
-.token()
+api.ticket().then(function(ticket){
+  console.log(ticket);
+});
--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 const assert = require('assert');
 const WeChat = require('../');
 
-var wx = new WeChat({
+var api = new WeChat({
   appId     : 'wxfb9dc00461bcef17',
   appSecret : '21f48e4c7c68683b681ca81ba60d3190'
 });
@@ -10,25 +10,16 @@ var wx = new WeChat({
 describe('wechat api', function() {
   
   var openId = 'ozwcHuC792LGHpQ0dYFlYoA2Uh_c';
-  
-  before(function(done){
-    wx.getToken().then(function(token){
-      wx.token = token.access_token;
-      assert.ok(wx.token);
-      // console.log(wx.token);
-      done();
-    });
-  });
-  
+
   it('get ticket ', function(done) {
-    wx.getTicket(wx.token).then(function(ticket){
+    api.ticket().then(function(ticket){
       assert.ifError(ticket.errcode, ticket.errmsg);
       done();
     });
   });
   
   it('get callback ip', function(done) {
-    wx.getCallbackIP(wx.token).then(function(res){
+    api.callback_ip().then(function(res){
       assert.ok(res.ip_list)
       assert.ifError(res.errcode, res.errmsg);
       done()
@@ -36,7 +27,7 @@ describe('wechat api', function() {
   });
   
   it('get user', function(done) {
-    wx.getUser(wx.token, openId).then(function(user){
+    api.user(openId).then(function(user){
       // console.log(user);
       assert.ok(user.openid);
       assert.ifError(user.errcode, user.errmsg);
@@ -44,41 +35,66 @@ describe('wechat api', function() {
     });
   });
   
-  // it('send template message', function(done) {
-  //   var templateId = 'zVwzW_Sm7Ln8mrf2a74-3XgWbDMPYbXVcjEbf-aA75o';
-  //   var data = {
-  //     name: 'Lsong',
-  //     content: 'test'
-  //   };
-  //
-  //   wx.template_send(wx.token, openId, templateId, data, 'https://lsong.org').then(function(res){
-  //     assert.ifError(res.errcode, res.errmsg);
-  //     assert.ok(res.msgid)
-  //     done();
-  //   });
-  // });
-  //
-  // it('send custom message', function(done) {
-  //   wx.custom_send(wx.token, openId, 'text', { content: 'test' }).then(function(res){
-  //     assert.ifError(res.errcode, res.errmsg);
-  //     done();
-  //   });
-  // });
-  //
-  // it('menu list', function(done) {
-  //   wx.menu_list(wx.token).then(function(res){
-  //     assert.ok(res.is_menu_open);
-  //     assert.ok(res.selfmenu_info);
-  //     assert.ifError(res.errcode, res.errmsg);
-  //     done();
-  //   });
-  // });
-  //
   it('set user remark', function(done) {
-    wx.user_remark(wx.token, openId, 'remark').then(function(res){
+    api.user_remark(openId, 'remark').then(function(res){
+      // console.log(res);
       assert.ifError(res.errcode, res.errmsg);
       done();
     });
   });
-
+  
+  it('list users', function(done) {
+    api.users().then(function(res){
+      // console.log(res);
+      assert.ok(res.total);
+      assert.ok(res.count);
+      assert.ok(res.data);
+      assert.ok(res.next_openid);
+      done()
+    });
+  });
+  
+  it('fetch user info', function(done) {
+    api.users_info([ openId ]).then(function(res){
+      assert.equal(res.user_info_list.length, 1);
+      done();
+    });
+  });
+  
+  it('send template message', function(done) {
+    var templateId = 'zVwzW_Sm7Ln8mrf2a74-3XgWbDMPYbXVcjEbf-aA75o';
+    var data = {
+      name: 'Lsong',
+      content: 'test'
+    };
+  
+    api.template_send(templateId, data, 'https://lsong.org', openId).then(function(res){
+      assert.ifError(res.errcode, res.errmsg);
+      assert.ok(res.msgid)
+      done();
+    });
+  });
+  
+  // it('send custom message', function(done) {
+  //   api.custom_send(openId, 'text', { content: 'test' }).then(function(res){
+  //     // console.log(res);
+  //     assert.ifError(res.errcode, res.errmsg);
+  //     done();
+  //   });
+  // });
+  
+  it('menu list', function(done) {
+    api.menu_list().then(function(res){
+      // console.log(res);
+      assert.ok(res.is_menu_open);
+      assert.ifError(res.errcode, res.errmsg);
+      done();
+    });
+  });
+  //
+  // it('auth refresh token', function(done) {
+  //   api.auth_refresh().then(function(res){
+  //     console.log(res);
+  //   });
+  // });
 });
