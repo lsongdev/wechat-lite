dd4fb8cbdb38ea0bcd92efb9c3a3fe1f09a3310d
Lsong <song940@gmail.com>
2016-06-27 19:17:34 +0800

    modify

--- example/server.js
+++ example/server.js
@@ -4,7 +4,22 @@ const http = require('http');
 const WeChat  = require('../');
 
 var app = new WeChat.Server('token', function(err, message){
-  return message.Content;
+  // console.log(message);
+  switch (message.MsgType) {
+    case 'text':
+      return message.Content; // text
+      break;
+    case 'link':
+      return message.Url;
+      break;
+    case 'location':
+      return message.Label;
+      break;
+    
+    default:
+      return message;
+      break;
+  }
 });
 
 http.createServer(app).listen(3000);
