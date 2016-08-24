1b5919c63ec09c99817310b5f1ec5557b98a143c
Lsong <song940@gmail.com>
2016-08-24 18:20:29 +0800

    add demo

--- example/client.js
+++ example/client.js
@@ -1,31 +1,24 @@
 const WeChat = require('../');
 const config = require('kelp-config');
 
-const client = new WeChat.Client(config.wechat);
-
-client.uuid().then(function(uuid){
-  console.log('>', client.qrcode(uuid));
-
-  (function wait(){
-  
-    client.status(uuid).then(function(status){
-      switch (status.code) {
-        case 200:
-          client.login(status.redirect_uri).then(function(info){
-            console.log('> login success', info);
-            client.init(info).then(function(){
-              
-            });
-          });
-          break;
-        case 201:
-          console.log('> scan success');
-        default:
-          wait();
-          break;
-      }
-    });
-  
-  })();
-
+const client = new WeChat.Client({
+  appId: 'wx782c26e4c19acffb'
 });
+
+client.on('scan', function(){
+  console.log('scan success');
+});
+
+client.on('login', function(){
+  console.log('login success');
+});
+
+client
+.uuid()
+.then(client.printQrcode.bind(client))
+.then(client.wait       .bind(client))
+.then(client.login      .bind(client))
+.then(client.init       .bind(client))
+.then(function(info){
+  console.log(info);
+})
